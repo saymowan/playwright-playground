22b173163de8ef8ae27ecac142110841ade08494
Saymon Oliveira <saymowan@gmail.com>
2022-07-21 10:10:09 +1000

    Implement ProductDetails test and test cases

---
+++ specs/productDetails.test.js
@@ -0,0 +1,27 @@
+
+//Given I'm on the departament store Main Page
+//When I access the product page
+//Then the page should display Data Sheet, More Info and Reviews of the product
+
+//Given I'm on the Product Page
+//When I share my product on the social media
+//Then the social media websites should be opened
+
+//Given I'm on the Product Page
+//When I update the Quantity 
+//And Add to card
+//Then the cart items and amount should be updated 
+
+//Given I'm on the Product Page non-signed-in
+//When I add the product to my wishlist
+//Then a alert should be display requesting the user to be signed in to perform Wishlist actions 
+
+//Given I'm on the Product Page
+//When I change the product color
+//Then the Product`s image should be updated
+
+//Functional testing
+
+//Non functional testing
+
+//Accessibility
